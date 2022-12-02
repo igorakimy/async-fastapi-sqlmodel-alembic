a1cc24a5a9bfaf902e80f50dc29366496e672b98
igorakimy <igorakimy@gmail.com>
2022-12-02 08:11:46 +0300

    add frontend

---
+++ frontend/src/api.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { apiUrl } from '@/env';
+import {
+  IUserProfile,
+  IUserProfileUpdate,
+  IUserProfileCreate
+} from './interfaces';
+
+function authHeaders(token: string) {
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+}
+
+export const api = {
+  async logInGetToken(username: string, password: string) {
+    const params = new URLSearchParams();
+    params.append('username', username);
+    params.append('password', password);
+    return axios.post(`${apiUrl}/login/token`, params);
+  },
+  async getMe(token: string) {
+    return axios.get<IUserProfile>(`${apiUrl}/users/me`, authHeaders(token));
+  },
+  async updateMe(token: string, data: IUserProfileUpdate) {
+    return axios.put<IUserProfile>(`${apiUrl}/users/me`, data, authHeaders(token));
+  },
+  async getUsers(token: string) {
+    return axios.get<IUserProfile[]>(`${apiUrl}/users/`, authHeaders(token));
+  },
+  async updateUser(token: string, userId: number, data: IUserProfileUpdate) {
+    return axios.put(`${apiUrl}/users/${userId}`, data, authHeaders(token));
+  },
+  async createUser(token: string, data: IUserProfileCreate) {
+    return axios.post(`${apiUrl}/users/`, data, authHeaders(token));
+  },
+  async passwordRecovery(email: string) {
+    return axios.post(`${apiUrl}/password-recovery/${email}`);
+  },
+  async resetPassword(password: string, token: string) {
+    return axios.post(`${apiUrl}/reset-password/`, {
+      new_password: password,
+      token,
+    });
+  },
+};
