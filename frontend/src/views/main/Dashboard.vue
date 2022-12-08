f522a718ed16b20c86dc5f5d872be7b06060e881
igorakimy <igorakimy@gmail.com>
2022-12-08 08:29:52 +0300

    change dashboard page

--- frontend/src/views/main/Dashboard.vue
+++ frontend/src/views/main/Dashboard.vue
@@ -2,15 +2,15 @@
   <v-container fluid>
     <v-card class="ma-3 pa-3">
       <v-card-title primary-title>
-        <div class="headline primary--text">Dashboard</div>
+        <div class="headline primary--text">Панель управления</div>
       </v-card-title>
       <v-card-text>
-        <div class="headline font-weight-light ma-5">Welcome {{greetedUser}}</div>
+        <div class="headline font-weight-light ma-5">Добро пожаловать, {{greetedUser}}</div>
       </v-card-text>
       <v-card-actions>
-        <v-btn to="/main/profile/view">View Profile</v-btn>
-        <v-btn to="/main/profile/edit">Edit Profile</v-btn>
-        <v-btn to="/main/profile/password">Change Password</v-btn>
+        <v-btn to="/main/profile/view">Профиль</v-btn>
+        <v-btn to="/main/profile/edit">Изменить профиль</v-btn>
+        <v-btn to="/main/profile/password">Изменить пароль</v-btn>
       </v-card-actions>
     </v-card>
   </v-container>
@@ -25,8 +25,8 @@ export default class Dashboard extends Vue {
   get greetedUser() {
     const userProfile = readUserProfile(this.$store);
     if (userProfile) {
-      if (userProfile.full_name) {
-        return userProfile.full_name;
+      if (userProfile.first_name) {
+        return userProfile.first_name + ' ' + userProfile.last_name;
       } else {
         return userProfile.email;
       }
