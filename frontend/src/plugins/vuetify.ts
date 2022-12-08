cdcd9c5d82280d53f09826f9944686744c91cded
igorakimy <igorakimy@gmail.com>
2022-12-08 19:45:18 +0300

    add translation for frontend validation

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -1,6 +1,22 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import VeeValidate, { Validator } from 'vee-validate';
+import ru from '@/locale/ru';
+
+
+Validator.localize({'ru': ru});
+
+Vue.use(VeeValidate, {
+    locale: 'ru'
+});
+
 
 Vue.use(Vuetify, {
     iconfont: 'md',
+    theme: {
+        primary: '#263238',
+        secondary: '#455A64',
+        accent: '#37474F',
+        error: '#b71c1c'
+    }
 });
